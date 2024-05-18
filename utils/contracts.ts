019b70d1221a322f09b388a455a54969fdb48d8f
dirdiebirdies <164374902+timesky404@users.noreply.github.com>
2024-05-18 04:38:59 -0600

    CA

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -4,8 +4,8 @@ import { getContract } from "thirdweb";
 import { stakingABI } from "./stakingABI";
 
 const nftContractAddress = "0xAf1DcBdB6c8F484C2B4c53b1EbA423b38BF17F8D";
-const rewardTokenContractAddress = "0x39073444AB20c5AF8941a972d97166BB1Dc7c1db";
-const stakingContractAddress = "0x2E801cdc8DaD167AA6d67A7814318B711e77aE89";
+const rewardTokenContractAddress = "0xd3dA4756E2633773eb049E641E3CCb355707F672";
+const stakingContractAddress = "0x973542C5Ab871B967D4419378c5165e9C3b56195";
 
 export const NFT_CONTRACT = getContract({
     client: client,
